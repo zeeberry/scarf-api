d9fe7282a88803a118cb0dbfc21bb4e987d57e68
zeeberry <zainabebrahimi@gmail.com>
2014-10-19 17:05:43 -0400

    creating a logger and added log messages

--- locationService.js
+++ locationService.js
@@ -4,7 +4,9 @@
 //
 //  Copyright (c) 2014 coffee.beans. All rights reserved.
 //
-	
+var logger = require('./logger');
+logger.debugLevel = 'info';
+
 var LocationService = function(lat, long) {
     this.errors = [];
 	this.latitude = this.setLatitude(lat);
@@ -24,73 +26,91 @@ var LocationService = function(lat, long) {
             location : null 
         }
 	};
+    
+    logger.log('info', 'LocationService instantiated');
 	
 };
 LocationService.prototype.setLatitude = function(latitude){
+    var errorMessage = 'Location Service - Attempt to set invalid latitude',
+        successMessage = 'Location Service - Set proper latitude';
 	try{
         if(latitude > -90 && latitude < 90){
+            logger.log('info', successMessage + ' ' + latitude);
             return latitude;
         }else {
-            throw new Error('invalid latitude');
+            throw new Error(errorMessage);
         }
     } catch(err){
-        this.setError('Invalid Latitude');       
+        logger.log('error', errorMessage);
+        this.setError(err.message);
     }
 };
 
 LocationService.prototype.setLongitude = function(longitude){
+    var errorMessage = 'Location Service - Attempt to set invalid longitude',
+        successMessage = 'Location Service - Set proper longitude';
     try {
         if(longitude > -180 && longitude < 180){
+            logger.log('info', successMessage + ' ' + longitude);
             return longitude;
         }else {
-            throw new Error('invalid longitude');
+            throw new Error(errorMessage);
         }	
     }catch(err) {
+        logger.log('error', errorMessage);
         this.setError(err.message);
     }
 };
 
 LocationService.prototype.getLongitude = function(){
+    logger.log('info', 'Location Service - Retrieving longitude ' + this.longitude);
 	return this.longitude;
 };
 
 LocationService.prototype.getLatitude = function(){
+    logger.log('info', 'Location Service - Retrieving latitude ' + this.latitude );
 	return this.latitude;
 };
 
 LocationService.prototype.getBaseUrl = function(){
+    logger.log('info', 'Location Service - Retrieving third party base url');
 	return this.baseUrl;
 };
 
 LocationService.prototype.getApiKey = function(){
+    logger.log('info', 'Location Service - Retrieving third party api key');
 	return this.apiKey;
 };
 
 LocationService.prototype.getPath = function(){
+    logger.log('info', 'Location Service - Retrieving third party api url path');
 	return this.path;
 };
 
 LocationService.prototype.setPath = function(){
+    logger.log('info', 'Location Service - Building third party url path');
 	return '/' + ['api', this.apiKey,this.endPoint, 'q'].join('/') +'/'+[this.latitude, this.longitude].join(',')+'.json';
 };
 
-LocationService.prototype.httpRequest = function(cb) {
+LocationService.prototype.httpRequest = function( cb ) {
+    logger.log('info', 'Location Service - Making HTTP request to third party');
 	var that = this,
 	    http = require('http'),
 	    options = {
-		host: this.baseUrl,
-		path: this.path
+            host: this.baseUrl,
+            path: this.path
 	    },
 	    callback = function(response) {
-		var str = '',
-		    json = '';
-		response.on('data', function(chunk){
-			str += chunk;
-		});
-		response.on('end', function(){
-			json = JSON.parse(str);
-			cb(json, that);
-		});
+            var str = '',
+                json = '';
+            response.on('data', function(chunk){
+                str += chunk;
+            });
+            response.on('end', function(){
+                json = JSON.parse(str);
+                cb(json, that);
+                logger.log('info', 'Location Service - HTTP request complete');
+            });
 	    };
 	
 	http.request(options, callback).end();
@@ -103,6 +123,7 @@ LocationService.prototype.getError = function(){
     return this.errors;
 };  
 LocationService.prototype.setScarf = function(res, that) {
+    logger.log('info', 'Location Service - Building Scarf Object');
     var obj = !!that ? that : this;
 
     if(obj.errors.length > 0){
@@ -120,6 +141,7 @@ LocationService.prototype.setScarf = function(res, that) {
 };
 
 LocationService.prototype.getScarf = function(){
+    logger.log('info', 'Location Service - Retrieving Scarf Object');  
 	return this.scarf;
 };
 
@@ -127,16 +149,20 @@ LocationService.prototype.setShouldWearScarf = function(response){
 	var wind_min = response.current_observation.wind_mph,
 	    wind_max = response.current_observation.wind_gust_mph;
 	this.scarf.data.should_wear_scarf = (wind_min + wind_max)/2 > 15 ? true : false;
+    logger.log('info', 'Location Service - Setting shouldWearScarf');
 };
 LocationService.prototype.getShouldWearScarf = function() {
+    logger.log('info', 'Location Service - Retrieving shouldWearScarf');
 	return this.scarf.data.should_wear_scarf;
 };
 
 LocationService.prototype.setDisplayLocation = function(response) {
-      this.scarf.data.location = response.current_observation.display_location.full;
+    this.scarf.data.location = response.current_observation.display_location.full;
+    logger.log('info', 'Location Service - Setting displayLocation');
 };
 
 LocationService.prototype.getDisplayLocation = function(){
+    logger.log('info', 'Location Service - Retrieving displayLocation');
 	return this.scarf.data.location;
 };
 
@@ -144,9 +170,11 @@ LocationService.prototype.setAvgWindMph = function(response){
 	var wind_min = response.current_observation.wind_mph,
         wind_max = response.current_observation.wind_gust_mph;	  
 	this.scarf.data.average_wind_speed.mph = (wind_min + wind_max)/2;
+    logger.log('info', 'Location Service - Setting avgWindMph');
 };
 
 LocationService.prototype.getAvgWindMph = function() {
+    logger.log('info', 'Location Service - Retrieving avgWindMph');
     return this.scarf.data.average_wind_speed.mph;
 };
 
@@ -154,9 +182,11 @@ LocationService.prototype.setAvgWindKph = function(response){
 	var wind_min = response.current_observation.wind_kph,
         wind_max = response.current_observation.wind_gust_kph;
     this.scarf.data.average_wind_speed.kph =  (wind_min + wind_max)/2;
+    logger.log('info', 'Location Service - Setting avgWindKph');
 };
 
 LocationService.prototype.getAvgWindKph = function(){
+    logger.log('info', 'Location Service - Retrieving avgWindKph');
     return this.scarf.data.average_wind_speed.kph;
 };  
 
